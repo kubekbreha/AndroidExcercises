376cab0e4c0f48efe9e5b571bb009fb26805593a
kubekbreha <kubek.breha@gmail.com>
2018-03-25 08:00:23 +0200

    base widget

---
+++ MyGardenWidget/app/src/main/java/excercise/kubo/sk/mygardenwidget/MainActivity.java
@@ -0,0 +1,13 @@
+package excercise.kubo.sk.mygardenwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
