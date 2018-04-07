be1f2cbe277f5b0f26f8eb43cf783f77764af4da
Jakub Brehuv <kubek.breha@gmail.com>
2018-04-07 11:23:34 +0200

    get togen from  cloum cloud aplication

--- IBMMFFandroidTest/app/src/main/java/com/example/jakubbrehuv/ibm_mffandroidtest/MainActivity.java
+++ IBMMFFandroidTest/app/src/main/java/com/example/jakubbrehuv/ibm_mffandroidtest/MainActivity.java
@@ -6,16 +6,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.worklight.wlclient.api.WLAccessTokenListener;
+import com.worklight.wlclient.api.WLAuthorizationManager;
+import com.worklight.wlclient.api.WLClient;
+import com.worklight.wlclient.api.WLFailResponse;
+import com.worklight.wlclient.auth.AccessToken;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.submit_button);
-        TextView textView = findViewById(R.id.text_view);
+        WLClient.createInstance(this);
+
+        button = findViewById(R.id.submit_button);
+        textView = findViewById(R.id.text_view);
 
         button.setTransformationMethod(null);
         button.setOnClickListener(this);
@@ -27,6 +37,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.submit_button ){
+            WLAuthorizationManager.getInstance().obtainAccessToken("", new WLAccessTokenListener() {
+                @Override
+                public void onSuccess(AccessToken accessToken) {
+                    System.out.println("received acces token is " + accessToken);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText("sucess");
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailure(WLFailResponse wlFailResponse) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText("failed");
+
+                        }
+                    });
+                }
+            });
 
         }
     }
