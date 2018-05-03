d6e8323d8fc205309699a739eb8be665a8dd095d
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-03 19:25:06 +0200

    finish no sense app

--- Kotlin/AppWithNoSense/app/src/main/java/com/nosense/grizzly/appwithnosense/MainActivity.kt
+++ Kotlin/AppWithNoSense/app/src/main/java/com/nosense/grizzly/appwithnosense/MainActivity.kt
@@ -2,11 +2,46 @@ package com.nosense.grizzly.appwithnosense
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val initialTextTranslationY = textView_progress.translationY
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView_progress.text = progress.toString()
+
+                val translationDistance = (initialTextTranslationY + progress * resources.getDimension(R.dimen.text_anim_step) * -1)
+
+                textView_progress.animate().translationY(translationDistance)
+
+                if(!fromUser) 
+                    textView_progress.animate().setDuration(500).rotationBy(360f).translationY(initialTextTranslationY)
+
+            }
+
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+
+        button_reset.setOnClickListener{ v: View ->
+            seekBar.progress = 0
+
+        }
     }
 }
