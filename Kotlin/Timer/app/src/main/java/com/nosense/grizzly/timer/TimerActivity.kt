3b0dd755c61e275607f21cdf7857497e6420c7d4
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-04 16:23:58 +0200

    timer core finished

--- Kotlin/Timer/app/src/main/java/com/nosense/grizzly/timer/TimerActivity.kt
+++ Kotlin/Timer/app/src/main/java/com/nosense/grizzly/timer/TimerActivity.kt
@@ -1,13 +1,16 @@
 package com.nosense.grizzly.timer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.nosense.grizzly.timer.util.PrefUtil
 
 import kotlinx.android.synthetic.main.activity_timer.*
+import kotlinx.android.synthetic.main.content_timer.*
 
 class TimerActivity : AppCompatActivity() {
 
@@ -66,8 +69,109 @@ class TimerActivity : AppCompatActivity() {
             //todo: show notification
 
         }
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
 
     }
+
+    private fun initTimer() {
+        timerState = PrefUtil.getTimerState(this)
+
+        if (timerState == TimerState.STOPPED)
+            setNewTimerLength()
+        else
+            setPreviusTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.RUNNING || timerState == TimerState.PAUSED)
+            PrefUtil.getSecondsRemaining(this)
+        else
+            timerLengthSeconds
+
+        //TODO: change secondsRemaining according to where the background timer stopped
+
+        //resume where we left off
+        if (timerState == TimerState.RUNNING)
+            startTimer()
+
+        updateButtons()
+        updateCountDownUI()
+
+    }
+
+
+    private fun onTimerFinnished() {
+        timerState = TimerState.STOPPED
+
+        setNewTimerLength()
+
+        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountDownUI()
+    }
+
+    private fun startTimer() {
+        timerState = TimerState.RUNNING
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountDownUI()
+            }
+
+            override fun onFinish() = onTimerFinnished()
+        }.start()
+    }
+
+
+    private fun setNewTimerLength() {
+        val lengthInMinutes = PrefUtil.getTimerLength(this)
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviusTimerLength() {
+        timerLengthSeconds = PrefUtil.getPrevoiusTimerLengthSeconds(this)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun updateCountDownUI() {
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        textView_countdown.text = "$minutesUntilFinished:${
+        if (secondsStr.length == 2) secondsStr
+        else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+
+    private fun updateButtons() {
+        when (timerState) {
+            TimerState.RUNNING -> {
+                fab_start.isEnabled = false
+                fab_pause.isEnabled = true
+                fab_stop.isEnabled = true
+            }
+            TimerState.STOPPED -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = false
+            }
+            TimerState.PAUSED -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = true
+            }
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
