aa5ac3b4b7a5f1d74771c848fabcdf744d04ce25
Jakub Brehuv <kubek.breha@gmail.com>
2018-05-07 11:00:31 +0200

    anko done

--- Kotlin/Anko/app/src/main/java/com/nosense/grizzly/anko/MainActivity.kt
+++ Kotlin/Anko/app/src/main/java/com/nosense/grizzly/anko/MainActivity.kt
@@ -2,11 +2,13 @@ package com.nosense.grizzly.anko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.appcompat.v7.contentFrameLayout
+import org.jetbrains.anko.setContentView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
     }
 }
